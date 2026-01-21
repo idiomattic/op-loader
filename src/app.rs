4226606b547a0ebc29dce01284b41082db416ef0
Matt Lese <matthewjlese@gmail.com>
2026-01-20 16:12:38 -0800

    adds vault_list_state to App

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use ratatui::widgets::ListState;
+
 #[derive(Debug, Clone)]
 pub struct Vault {
     pub id: String,
@@ -8,6 +10,8 @@ pub struct App {
     pub should_quit: bool,
 
     pub vaults: Vec<Vault>,
+
+    pub vault_list_state: ListState,
 }
 
 impl App {
@@ -15,6 +19,7 @@ impl App {
         let mut app = Self {
             should_quit: false,
             vaults: Vec::new(),
+            vault_list_state: ListState::default(),
         };
         app
     }
