3b03b9dbda6ce65370e0c22b88c9bb794776ca59
Matt Lese <matthewjlese@gmail.com>
2026-01-20 16:16:26 -0800

    sets up render function

--- src/ui.rs
+++ src/ui.rs
@@ -1,5 +1,17 @@
-use ratatui::Frame;
+use ratatui::{
+    Frame,
+    layout::{Constraint, Direction, Layout, Rect},
+};
 
 use crate::app::App;
 
-pub fn render(frame: &mut Frame, app: &mut App) {}
+pub fn render(frame: &mut Frame, app: &mut App) {
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(30), Constraint::Percentage(70)])
+        .split(frame.area());
+
+    render_vault_list(frame, app, chunks[0]);
+}
+
+fn render_vault_list(frame: &mut Frame, app: &mut App, area: Rect) {}
