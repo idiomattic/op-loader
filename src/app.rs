33b333e6b9e5b7b18161c6587e0b267e174b4403
Matt Lese <matthewjlese@gmail.com>
2026-01-20 16:07:24 -0800

    removes App::draw method

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,3 @@
-use ratatui::Frame;
-
 pub struct App {
     pub should_quit: bool,
 }
@@ -8,9 +6,5 @@ impl App {
     pub fn new() -> Self {
         let mut app = Self { should_quit: false };
         app
-    }
-
-    pub fn draw(&self, frame: &mut Frame) {
-        todo!()
     }
 }
