3bbe6d6662b79877b35e6dce5004a33c04f73377
Matt Lese <matthewjlese@gmail.com>
2026-01-20 16:09:17 -0800

    adds vaults field to App

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,21 @@
+#[derive(Debug, Clone)]
+pub struct Vault {
+    pub id: String,
+    pub name: String,
+}
+
 pub struct App {
     pub should_quit: bool,
+
+    pub vaults: Vec<Vault>,
 }
 
 impl App {
     pub fn new() -> Self {
-        let mut app = Self { should_quit: false };
+        let mut app = Self {
+            should_quit: false,
+            vaults: Vec::new(),
+        };
         app
     }
 }
