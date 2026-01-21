e8ddd5a045bbaaf26a7f1afa74ca494ab2848eb4
Matt Lese <matthewjlese@gmail.com>
2026-01-20 16:05:03 -0800

    handles quit events

--- src/event.rs
+++ src/event.rs
@@ -1,7 +1,22 @@
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use std::io;
 
 use crate::app::App;
 
+fn handle_key_press(app: &mut App, key: KeyEvent) {
+    match key.code {
+        KeyCode::Char('q') | KeyCode::Char('Q') => {
+            app.should_quit = true;
+        }
+        _ => {}
+    }
+}
+
 pub fn handle_events(app: &mut App) -> io::Result<()> {
-    todo!()
+    if let Event::Key(key) = event::read()? {
+        if key.kind == KeyEventKind::Press {
+            handle_key_press(app, key);
+        }
+    }
+    Ok(())
 }
